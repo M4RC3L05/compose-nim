e121cd69b41ef94c6fc0916f1d10c81476b700f6
M4RC3L0 <joaobat@live.com.pt>
2018-09-22 02:46:29 +0100

    remove echo from test file

--- composeTests.nim
+++ composeTests.nim
@@ -6,22 +6,21 @@ suite "Compose Tests":
     setup:
         proc addOne(x: int): int=
             return x + 1
-        
+
         proc multiplyTwo(x: int): int=
             return x * 2
-        
+
         proc subtractThree(x: int): int=
             return x - 3
-        
+
         proc addFour(x: int): int=
-            echo("oioioi")
             return x + 4
 
     test "it should compose functions":
         let res = compose[int](
             addOne,
-            multiplyTwo, 
-            subtractThree, 
+            multiplyTwo,
+            subtractThree,
             addFour,
         )(0)
 
