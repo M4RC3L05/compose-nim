dba515600fe84488af14525443a9023aafaaab3e
M4RC3L0 <joaobat@live.com.pt>
2018-09-22 02:42:42 +0100

    compose function

---
+++ composeTests.nim
@@ -0,0 +1,28 @@
+import unittest
+import ./compose
+
+suite "Compose Tests":
+
+    setup:
+        proc addOne(x: int): int=
+            return x + 1
+        
+        proc multiplyTwo(x: int): int=
+            return x * 2
+        
+        proc subtractThree(x: int): int=
+            return x - 3
+        
+        proc addFour(x: int): int=
+            echo("oioioi")
+            return x + 4
+
+    test "it should compose functions":
+        let res = compose[int](
+            addOne,
+            multiplyTwo, 
+            subtractThree, 
+            addFour,
+        )(0)
+
+        check(res == 3)
